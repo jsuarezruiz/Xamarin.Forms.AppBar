99d32731fe88b39031a287d60697eb15f44c0a2f
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-03-24 11:48:44 +0100

    Added empty project

---
+++ src/AppBar.Sample/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using AppBar.Sample.Views;
+using Xamarin.Forms;
+
+namespace AppBar.Sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
